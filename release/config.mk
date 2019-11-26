c35ea91374326a42e5318bd5e81c7227dcacb183
Sam Salisbury <samsalisbury@gmail.com>
2019-11-26 10:25:23 +0000

    release: update to use packagespec tool

--- release/config.mk
+++ release/config.mk
@@ -12,6 +12,68 @@ CONFIG_INCLUDED := YES
 # Set SHELL to strict mode, in a way compatible with both old and new GNU make.
 SHELL := /usr/bin/env bash -euo pipefail -c
 
+# RELEASE_DIR is the path to the dir containing all the
+# release makefiles etc., relative from the repo root.
+# Typically this is 'release'.
+RELEASE_DIR := release
+
+# CACHE_ROOT is the build cache directory.
+CACHE_ROOT ?= .buildcache
+
+# SPEC is the human-managed description of which packages we are able to build.
+SPEC := packages.yml
+
+# LOCKDIR contains the lockfile and layer files.
+LOCKDIR := packages.lock
+
+# LOCK is the generated fully-expanded rendition of SPEC, for use in generating CI
+# pipelines and other things.
+LOCK := $(LOCKDIR)/pkgs.yml
+
+# PACKAGE_CACHE_KEY_FILES is the place we write package cache key files.
+PACKAGE_CACHE_KEY_FILES := .tmp/cache-keys
+
+# ALWAYS_EXCLUDE_SOURCE prevents source from these directories from taking
+# part in the SOURCE_ID, or from being sent to the builder image layers.
+# This is important for allowing the head of master to build other commits
+# where this build system has not been vendored.
+#
+# Source in RELEASE_DIR is encoded as PACKAGE_SPEC_ID and included in paths
+# and cache keys. Source in .circleci/ should not do much more than call
+# code in the release/ directory.
+ALWAYS_EXCLUDE_SOURCE     := $(RELEASE_DIR)/ .circleci/
+# ALWAYS_EXCLUD_SOURCE_GIT is git path filter parlance for the above.
+ALWAYS_EXCLUDE_SOURCE_GIT := ':(exclude)$(RELEASE_DIR)/' ':(exclude).circleci/'
+
+# Even though layers may have different Git revisions, based on the latest
+# revision of their source, we always want to
+# honour either HEAD or the specified PRODUCT_REVISION for compiling the
+# final binaries, as this revision is the one picked by a human to form
+# the release.
+ifeq ($(PRODUCT_REVISION),)
+# If PRODUCT_REVISION is empty (the default) we are concerned with building the
+# current work tree, regardless of whether it is dirty or not. For local builds
+# this is more convenient and more likely expected behaviour than having to commit
+# just to perform a new build.
+GIT_REF := HEAD
+ALLOW_DIRTY ?= YES
+else
+# PRODUCT_REVISION is non-emtpy so treat it as a git commit ref and pull files
+# directly from git rather than the work tree.
+GIT_REF := $(PRODUCT_REVISION)
+ALLOW_DIRTY := NO
+endif
+
+# Determine the PACKAGE_SOURCE_ID.
+ifeq ($(ALLOW_DIRTY),YES)
+DIRTY := $(shell git diff --exit-code $(GIT_REF) -- $(ALWAYS_EXCLUDE_SOURCE_GIT) > /dev/null 2>&1 || echo "dirty_")
+PACKAGE_SOURCE_ID := $(DIRTY)$(shell git rev-parse $(GIT_REF))
+else
+PACKAGE_SOURCE_ID := $(shell git rev-parse $(GIT_REF))
+endif
+
+
+
 # We rely on GNU touch and tar. On macOS, we assume they are installed as gtouch and gtar
 # by homebrew.
 ifeq ($(shell uname),Darwin)
@@ -55,61 +117,9 @@ QUOTE_LIST = $(addprefix $(QUOTE),$(addsuffix $(QUOTE),$(1)))
 GIT_EXCLUDE_LIST = $(call QUOTE_LIST,$(addprefix $(1)))
 ### End utilities and constants.
 
-# RELEASE_DIR is the path to the dir containing all the
-# release makefiles etc. typically this is 'release'.
-RELEASE_DIR := $(shell dirname $(lastword $(MAKEFILE_LIST)))
 
-# CACHE_ROOT is the build cache directory.
-CACHE_ROOT ?= .buildcache
 
-# SPEC is the human-managed description of which packages we are able to build.
-SPEC := packages.yml
 
-# LOCK is the generated fully-expanded rendition of SPEC, for use in generating CI
-# pipelines and other things.
-LOCK := packages.lock
-
-# PACKAGE_CACHE_KEY_FILES is the place we write package cache key files.
-PACKAGE_CACHE_KEY_FILES := .tmp/cache-keys
-
-# ALWAYS_EXCLUDE_SOURCE prevents source from these directories from taking
-# part in the SOURCE_ID, or from being sent to the builder image layers.
-# This is important for allowing the head of master to build other commits
-# where this build system has not been vendored.
-#
-# Source in RELEASE_DIR is encoded as PACKAGE_SPEC_ID and included in paths
-# and cache keys. Source in .circleci/ should not do much more than call
-# code in the release/ directory.
-ALWAYS_EXCLUDE_SOURCE     := $(RELEASE_DIR)/ .circleci/
-# ALWAYS_EXCLUD_SOURCE_GIT is git path filter parlance for the above.
-ALWAYS_EXCLUDE_SOURCE_GIT := ':(exclude)$(RELEASE_DIR)/' ':(exclude).circleci/'
-
-# Even though layers may have different Git revisions, based on the latest
-# revision of their source, we always want to
-# honour either HEAD or the specified PRODUCT_REVISION for compiling the
-# final binaries, as this revision is the one picked by a human to form
-# the release.
-ifeq ($(PRODUCT_REVISION),)
-# If PRODUCT_REVISION is empty (the default) we are concerned with building the
-# current work tree, regardless of whether it is dirty or not. For local builds
-# this is more convenient and more likely expected behaviour than having to commit
-# just to perform a new build.
-GIT_REF := HEAD
-ALLOW_DIRTY ?= YES
-else
-# PRODUCT_REVISION is non-emtpy so treat it as a git commit ref and pull files
-# directly from git rather than the work tree.
-GIT_REF := $(PRODUCT_REVISION)
-ALLOW_DIRTY := NO
-endif
-
-# Determine the PACKAGE_SOURCE_ID.
-ifeq ($(ALLOW_DIRTY),YES)
-DIRTY := $(shell git diff --exit-code $(GIT_REF) -- $(ALWAYS_EXCLUDE_SOURCE_GIT) > /dev/null 2>&1 || echo "dirty_")
-PACKAGE_SOURCE_ID := $(DIRTY)$(shell git rev-parse $(GIT_REF))
-else
-PACKAGE_SOURCE_ID := $(shell git rev-parse $(GIT_REF))
-endif
 
 # End including config once only.
 endif
