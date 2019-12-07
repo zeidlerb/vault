8543adbb82cde51786fe5fa800c1127ffb47e928
Sam Salisbury <samsalisbury@gmail.com>
2019-12-07 10:57:09 +0000

    release: less verbose go install, remove gox

---
+++ release/packages.lock/layers/ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83.mk
@@ -0,0 +1,9 @@
+
+LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_ID             := ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83
+LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_BASE_LAYER     := install-yarn_2924ca2287eec8fe0c29c98a3547069f7ef7d1d5
+LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_SOURCE_INCLUDE := ui/package.json ui/yarn.lock
+LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_SOURCE_EXCLUDE := 
+LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_CACHE_KEY_FILE := .buildcache/cache-keys/ui-dependencies-f7572cf365fa287729ae3dbd838d54c1d4a4ad83
+LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_ARCHIVE_FILE   := .buildcache/archives/ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83.tar.gz
+$(eval $(call LAYER,$(LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_ID),$(LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_BASE_LAYER),$(LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_SOURCE_INCLUDE),$(LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_SOURCE_EXCLUDE),$(LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_CACHE_KEY_FILE),$(LAYER_ui-dependencies_f7572cf365fa287729ae3dbd838d54c1d4a4ad83_ARCHIVE_FILE)))
+
