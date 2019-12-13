3c7b4ba69b960417300059329b65cfa5ac60890c
Sam Salisbury <samsalisbury@gmail.com>
2019-12-13 23:11:02 +0000

    release: better config + build-ci
    - No longer need to clone the releaser repo by hand.

--- release/config-oss.mk
+++ release/config-oss.mk
@@ -1,14 +1,15 @@
 # config-oss.mk includes some additional config.
 # It is automatically included along with the main config.mk.
-build-ci: PRODUCT_NAME ?= vault
-build-ci: PRODUCT_REVISION ?= $(shell git rev-parse HEAD)
-build-ci: PRODUCT_VERSION ?= 0.0.0-$(USER)-snapshot
-build-ci: PRODUCT_REPO ?= git@github.com:hashicorp/vault.git
-build-ci: PRODUCT_CIRCLECI_SLUG ?= gh/hashicorp/vault
-build-ci: PRODUCT_CIRCLECI_HOST ?= circleci.com
-build-ci: RELEASE_SYSTEM_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
 
-export PRODUCT_NAME PRODUCT_REVISION PRODUCT_VERSION PRODUCT_REPO PRODUCT_CIRCLECI_SLUG PRODUCT_CIRCLECI_HOST RELEASE_SYSTEM_BRANCH
+# PRODUCT_REPO is the official Git repo for this project.
+PRODUCT_REPO := git@github.com:hashicorp/vault.git
 
-# SPEC and LOCKDIR should already be set by the main config file.
-export SPEC LOCKDIR
+# PRODUCT_PATH must be unique for every repo.
+# A golang-style package path is ideal.
+PRODUCT_PATH := github.com/hashicorp/vault
+
+# PRODUCT_CIRCLECI_SLUG is the slug of this repo's CircleCI project.
+PRODUCT_CIRCLECI_SLUG ?= gh/hashicorp/vault
+
+# PRODUCT_CIRCLECI_HOST is the host configured to build this repo.
+PRODUCT_CIRCLECI_HOST ?= circleci.com
